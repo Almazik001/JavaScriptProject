57f14ae1788fb7b284077dcf933160017d6f12cf
Almazik001 <almazshasaitov169@gmail.com>
2024-02-05 14:58:17 +0600

    Модальное окно

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import PostList from "./companent/PostList.jsx";
 import PostForm from "./companent/PostForm.jsx";
+import MyModal from "./companent/UI/MyModal/MyModal.jsx";
+import MyButton from "./companent/UI/MyButton/MyButton.jsx";
 
 function App() {
 
   const [posts, setPosts] = useState([])
+  const [modal, setModal] = useState(false)
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -17,7 +21,12 @@ function App() {
 
   return (
     <div className='App'>
-      <PostForm create={createPost}/>
+        <MyButton onClick={() => setModal(true)}>
+            Создать пользователя
+        </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
         {posts.length !== 0
             ?
             <PostList posts={posts} remove={removePost} title='JavaScript'/>
